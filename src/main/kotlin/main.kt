3d36e92638e724facea8a0a98a7edcb06a556a1c
lazaroofarrill <lazaroofarrill@gmail.com>
2021-05-22 03:19:17 -0400

    window dragging and fullscreen

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,20 +9,24 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import views.ControlBar
 import views.PlayerFrame
 import views.Playlist
+import java.awt.Dimension
 
 @ExperimentalFoundationApi
 fun main(args: Array<String>) = Window {
 
-    val window = LocalAppWindow.current
+    val currentWindow = LocalAppWindow.current
     val setFullScreen = remember { mutableStateOf(false) }
     if (setFullScreen.value) {
-        window.makeFullscreen()
+        currentWindow.makeFullscreen()
     } else {
-        window.restore()
+        currentWindow.restore()
     }
+
+    currentWindow.window.minimumSize = Dimension(600, 480)
 
     var videoFile = "/home/lazaroofarrill/Videos/Big_Buck_Bunny_first_23_seconds_1080p.ogv"
     if (args.isNotEmpty()) {
@@ -39,12 +43,12 @@ fun main(args: Array<String>) = Window {
 
     val showPlaylist: MutableState<Boolean> = remember { mutableStateOf(false) }
     val mediaPlayerComponent =
-        remember { mutableStateOf(KryerMediaPlayerComponent(videoPosition, playing, setFullScreen, window)) }
+        remember { mutableStateOf(KryerMediaPlayerComponent(videoPosition, playing, setFullScreen, currentWindow)) }
     val readyToPlay = remember { mutableStateOf(false) }
 
     MaterialTheme {
         Column(
-            modifier = modifier.fillMaxSize()
+            modifier = modifier.fillMaxSize().defaultMinSize(600.dp, 480.dp)
         ) {
             Row(modifier.weight(1f).fillMaxWidth()) {
                 PlayerFrame(
